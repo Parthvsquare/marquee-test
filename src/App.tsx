c01f627393e3a0fc9880205ebe7a0704ae4459d9
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2023-06-13 22:42:53 +0530

    feat: auth with remember me completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { ContextStore } from "./Store";
 import AppRouter from "./routes";
 
 function App() {
+  localStorage.setItem("email", "marquee@gmail.com");
+  localStorage.setItem("password", "qwert@12345");
+
   return (
     <ContextStore>
       <AppRouter />
--- src/Pages/Auth/index.tsx
+++ src/Pages/Auth/index.tsx
@@ -1,27 +1,80 @@
 import { useStore } from "@/Store";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-interface IFormInput {
-  email: string;
-  password: string;
-}
+const inputSchema = z.object({
+  email: z
+    .string()
+    .nonempty({ message: "Email should cannot be empty" })
+    .email({ message: "Not a email field" }),
+  password: z
+    .string()
+    .nonempty({ message: "Password should cannot be empty" })
+    .min(8, { message: "Password should be greater than 8 character" }),
+  checkBox: z.boolean(),
+});
+
+type IFormInput = z.infer<typeof inputSchema>;
 
 function AuthPage() {
   const { dispatch } = useStore();
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
-  const loginUser = () => {
-    dispatch({
-      type: "SET_USER_AUTH",
-      payload: true,
-    });
-    localStorage.setItem("auth", JSON.stringify(true));
-    navigate("/");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    reset,
+  } = useForm<IFormInput>({
+    resolver: zodResolver(inputSchema),
+  });
+
+  const authentication = ({
+    inputEmailId,
+    inputPassword,
+  }: {
+    inputEmailId: string;
+    inputPassword: string;
+  }) => {
+    const emailId = localStorage.getItem("email");
+    const password = localStorage.getItem("password");
+    if (inputEmailId === emailId && password === inputPassword) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    const response = authentication({
+      inputEmailId: data.email,
+      inputPassword: data.password,
+    });
+    if (response) {
+      dispatch({
+        type: "SET_USER_AUTH",
+        payload: true,
+      });
+      if (data.checkBox) {
+        localStorage.setItem("auth", JSON.stringify(true));
+      }
+      navigate("/");
+    } else {
+      setError(
+        "email",
+        { type: "focus", message: "Incorrect Credentials" },
+        { shouldFocus: true },
+      );
+      reset({ password: "" }, { keepErrors: true });
+    }
+  };
 
   return (
     <div className="flex h-[calc(100vh_-_64px)] w-screen items-center justify-center">
@@ -40,25 +93,49 @@ function AuthPage() {
             </label>
             <input
               type="email"
-              name="email"
-              id="email"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+              {...register("email", { required: true })}
+              className={`block w-full rounded-lg border ${
+                errors.email?.message ? "border-red-500" : "border-gray-300"
+              }
+              bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500
+              focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600
+              dark:text-white dark:placeholder-gray-400`}
               placeholder="name@company.com"
               required
             />
+            {errors.email && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                <span className="font-medium">{errors.email.message}</span>
+              </p>
+            )}
           </div>
-          <div>
+          <div className="relative">
             <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
               Your password
             </label>
+            <div
+              className="absolute inset-y-12 right-3 flex cursor-pointer items-center pl-3"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+            </div>
             <input
-              type="password"
-              name="password"
-              id="password"
+              type={showPassword ? "text" : "password"}
+              {...register("password", { required: true })}
               placeholder="••••••••"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
               required
+              className={`block w-full rounded-lg border ${
+                errors.password?.message ? "border-red-500" : "border-gray-300"
+              }
+              bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500
+              focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600
+              dark:text-white dark:placeholder-gray-400`}
             />
+            {errors.password && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                <span className="font-medium">{errors.password.message}</span>
+              </p>
+            )}
           </div>
           <div className="flex items-start">
             <div className="flex items-start">
@@ -66,26 +143,18 @@ function AuthPage() {
                 <input
                   id="remember"
                   type="checkbox"
-                  value=""
+                  {...register("checkBox")}
                   className="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-                  required
                 />
               </div>
               <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Remember me
               </label>
             </div>
-            <a
-              href="#"
-              className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Lost Password?
-            </a>
           </div>
           <button
             type="submit"
             className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={loginUser}
           >
             Login to your account
           </button>
